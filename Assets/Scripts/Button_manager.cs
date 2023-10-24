5a0ec472b98766045fa2535551828040edbe1b0f
GoleaF3000 <93461021+GoleaF3000@users.noreply.github.com>
2023-10-24 19:51:56 +0700

    add button
    Добавлена анимированная кнопка, на ее основе создан префаб, и создан скрипт для включения Button UI, которая в свою очередь будет переключать сцену.

---
+++ Assets/Scripts/Button_manager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Mouse_click_manager))]
+public class Button_manager : MonoBehaviour
+{
+    [SerializeField] private Button _scene_change_button;
+
+    private Mouse_click_manager _mouse_manager;
+
+    private void Awake()
+    {
+        _mouse_manager = GetComponent<Mouse_click_manager>();
+    }
+
+    private void OnEnable()
+    {
+        _mouse_manager.Mouse_cliked += OnTurnOnButtonUI;
+    }
+
+    private void OnDisable()
+    {
+        _mouse_manager.Mouse_cliked -= OnTurnOnButtonUI;
+    }
+
+    private void OnTurnOnButtonUI()
+    {
+        //Debug.Log("CLICK");
+
+        if (_scene_change_button.gameObject.active == false)
+        {
+            _scene_change_button.gameObject.active = true;
+        }
+    }
+}
--- Assets/Scripts/Mouse_click_manager.cs
+++ Assets/Scripts/Mouse_click_manager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Mouse_click_manager : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _audio;
 
+    public event UnityAction Mouse_cliked;
+
     private void OnMouseDown()
     {
         _animator.SetTrigger(Constants.Animation.Mouse_click);
         _audio.Play();
+        Mouse_cliked?.Invoke();
     }
 }
