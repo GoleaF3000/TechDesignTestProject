0ac096c091fe727ed0da6cd49e11a279e84e030b
GoleaF3000 <93461021+GoleaF3000@users.noreply.github.com>
2023-10-24 18:35:51 +0700

    add sound effect and rename scrip
    Добавлен звуковой эффект монеты при нажатии. Переименован скрипт.

--- Assets/Scripts/Mouse_click_manager.cs
+++ Assets/Scripts/Mouse_click_manager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation_launch_manager : MonoBehaviour
+public class Mouse_click_manager : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private AudioSource _audio;
 
     private void OnMouseDown()
     {
         _animator.SetTrigger(Constants.Animation.Mouse_click);
+        _audio.Play();
     }
 }
