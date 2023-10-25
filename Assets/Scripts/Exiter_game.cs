ba2965ca07b92d138bdc02b9de28b555ded1432b
GoleaF3000 <93461021+GoleaF3000@users.noreply.github.com>
2023-10-25 19:37:29 +0700

    add: добавлена кнопка Выход
    Добавлена в UI кнопка выхода из игры. Добавлена локализация для этой кнопки. Созданы префабы кнопок Next и Exite, помещены в папку Prefabs > ui > buttons

---
+++ Assets/Scripts/Exiter_game.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exiter_game : MonoBehaviour
+{
+    public void Exite()
+    {
+        Application.Quit();
+    }
+}
